b956d5e6d367fc2201cbeb5a0b0c336de3d4a9df
bezerracdd <cdiegoll@gmail.com>
2026-02-17 13:38:32 -0300

    Descrição das alterações

---
+++ assets/src/pages/Home/sections/Hero/Hero.tsx
@@ -0,0 +1,28 @@
+import { styled } from "@mui/material/styles"
+import Avatar from "../../../../../src/assets/images/Profile_DB.jpg";
+
+const Hero = () => {
+
+    const Hero = () => {
+
+        const StyledHero = styled("div")(() => ({
+            blackgroundColor: "black"
+
+        }))
+
+        const StyledImg = styled("img")(() => ({
+            width: "30%",
+            borderRadius: "50%"
+        }))
+
+        return (
+            <>
+                <StyledHero>
+                    ola
+                    <StyledImg src={Avatar} />
+                </StyledHero>
+            </>
+        )
+    }
+
+    export default Hero
